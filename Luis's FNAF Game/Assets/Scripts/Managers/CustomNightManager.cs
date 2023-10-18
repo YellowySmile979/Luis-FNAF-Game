ac633317602be8be6ab65e2bf25aad0c2ee4939e
Elijah Boey <xcant5@gmail.com>
2023-10-18 14:42:40 +0800

    updated the mainm menu stuff more

--- Luis's FNAF Game/Assets/Scripts/Managers/CustomNightManager.cs
+++ Luis's FNAF Game/Assets/Scripts/Managers/CustomNightManager.cs
@@ -7,7 +7,14 @@ public class CustomNightManager : MonoBehaviour
 {
     [Header("AI Lvl Text")]
     public List<Text> aiLvlText = new List<Text>();
+
     public float miaLvl = 0f, shaunLvl = 0f, jadeLvl = 0f, enQiLvl = 0f, elijahLvl = 0f;
+
+    public const string miaKey = "Mia",
+                        shaunKey = "Shaun",
+                        jadeKey = "Jade",
+                        enQiKey = "En Qi",
+                        elijahKey = "Elijah";
 
     [Header("Image Opacity")]
 
@@ -17,7 +24,17 @@ public class CustomNightManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        aiLvlText.Find(s => s.CompareTag("Mia")).text = miaLvl.ToString();
+        aiLvlText.Find(s => s.CompareTag("Shaun")).text = shaunLvl.ToString();
+        aiLvlText.Find(s => s.CompareTag("Jade")).text = jadeLvl.ToString();
+        aiLvlText.Find(s => s.CompareTag("En Qi")).text = enQiLvl.ToString();
+        aiLvlText.Find(s => s.CompareTag("Elijah")).text = elijahLvl.ToString();
+
+        PlayerPrefs.SetFloat(miaKey, miaLvl);
+        PlayerPrefs.SetFloat(shaunKey, shaunLvl);
+        PlayerPrefs.SetFloat(jadeKey, jadeLvl);
+        PlayerPrefs.SetFloat(enQiKey, enQiLvl);
+        PlayerPrefs.SetFloat(elijahKey, elijahLvl);
     }
 
     // Update is called once per frame
@@ -26,60 +43,104 @@ public class CustomNightManager : MonoBehaviour
         
     }
 
-    public void IncreaseLvl()
+    public void IncreaseMiaLvl()
     {
-        if(FindObjectOfType<Text>().CompareTag("Mia"))
+        if (miaLvl < 20)
         {
-            if(miaLvl < 20) miaLvl++;
-            aiLvlText.Find(s => s.CompareTag("Mia")).text = miaLvl.ToString();
+            miaLvl++;
+            PlayerPrefs.SetFloat(miaKey, miaLvl);
         }
-        else if (FindObjectOfType<Text>().CompareTag("Shaun"))
-        {
-            if (shaunLvl < 20) shaunLvl++;
-            aiLvlText.Find(s => s.CompareTag("Shaun")).text = shaunLvl.ToString();
-        }
-        else if (FindObjectOfType<Text>().CompareTag("Jade"))
-        {
-            if (jadeLvl < 20) jadeLvl++;
-            aiLvlText.Find(s => s.CompareTag("Jade")).text = jadeLvl.ToString();
-        }
-        else if(FindObjectOfType<Text>().CompareTag("En Qi"))
-        {
-            if (enQiLvl < 20) enQiLvl++;
-            aiLvlText.Find(s => s.CompareTag("En Qi")).text = enQiLvl.ToString();
-        }
-        else if (FindObjectOfType<Text>().CompareTag("Elijah"))
-        {
-            if (elijahLvl < 20) elijahLvl++;
-            aiLvlText.Find(s => s.CompareTag("Elijah")).text = elijahLvl.ToString();
-        }
+
+        aiLvlText.Find(s => s.CompareTag("Mia")).text = PlayerPrefs.GetFloat(miaKey).ToString();
     }
-    public void DecreaseLvl()
+    public void DecreaseMiaLvl()
     {
-        if (FindObjectOfType<Text>().CompareTag("Mia"))
+        if (miaLvl > 0)
         {
-            if (miaLvl > 0) miaLvl--;
-            aiLvlText.Find(s => s.CompareTag("Mia")).text = miaLvl.ToString();
+            miaLvl--;
+            PlayerPrefs.SetFloat(miaKey, miaLvl);
         }
-        else if (FindObjectOfType<Text>().CompareTag("Shaun"))
+
+        aiLvlText.Find(s => s.CompareTag("Mia")).text = PlayerPrefs.GetFloat(miaKey).ToString();
+    }
+    public void IncreaseShaunLvl()
+    {
+        if (shaunLvl < 20)
         {
-            if (shaunLvl > 0) shaunLvl--;
-            aiLvlText.Find(s => s.CompareTag("Shaun")).text = shaunLvl.ToString();
+            shaunLvl++;
+            PlayerPrefs.SetFloat(shaunKey, shaunLvl);
         }
-        else if (FindObjectOfType<Text>().CompareTag("Jade"))
+
+        aiLvlText.Find(s => s.CompareTag("Shaun")).text = PlayerPrefs.GetFloat(shaunKey).ToString();
+    }
+    public void DecreaseShaunLvl()
+    {
+        if (shaunLvl > 0)
         {
-            if (jadeLvl > 0) jadeLvl--;
-            aiLvlText.Find(s => s.CompareTag("Jade")).text = jadeLvl.ToString();
+            miaLvl--;
+            PlayerPrefs.SetFloat(shaunKey, shaunLvl);
         }
-        else if (FindObjectOfType<Text>().CompareTag("En Qi"))
+
+        aiLvlText.Find(s => s.CompareTag("Shaun")).text = PlayerPrefs.GetFloat(shaunKey).ToString();
+    }
+    public void IncreaseJadeLvl()
+    {
+        if (jadeLvl < 20)
         {
-            if (enQiLvl > 0) enQiLvl--;
-            aiLvlText.Find(s => s.CompareTag("En Qi")).text = enQiLvl.ToString();
+            jadeLvl++;
+            PlayerPrefs.SetFloat(jadeKey, jadeLvl);
         }
-        else if (FindObjectOfType<Text>().CompareTag("Elijah"))
+
+        aiLvlText.Find(s => s.CompareTag("Jade")).text = PlayerPrefs.GetFloat(jadeKey).ToString();
+    }
+    public void DecreaseJadeLvl()
+    {
+        if (jadeLvl > 0)
         {
-            if (elijahLvl > 0) elijahLvl--;
-            aiLvlText.Find(s => s.CompareTag("Elijah")).text = elijahLvl.ToString();
+            jadeLvl--;
+            PlayerPrefs.SetFloat(jadeKey, jadeLvl);
         }
+
+        aiLvlText.Find(s => s.CompareTag("Jade")).text = PlayerPrefs.GetFloat(jadeKey).ToString();
+    }
+    public void IncreaseEnQiLvl()
+    {
+        if (enQiLvl < 20)
+        {
+            enQiLvl++;
+            PlayerPrefs.SetFloat(enQiKey, enQiLvl);
+        }
+
+        aiLvlText.Find(s => s.CompareTag("En Qi")).text = PlayerPrefs.GetFloat(enQiKey).ToString();
+    }
+    public void DecreaseEnQiLvl()
+    {
+        if (enQiLvl > 0)
+        {
+            enQiLvl--;
+            PlayerPrefs.SetFloat(enQiKey, enQiLvl);
+        }
+
+        aiLvlText.Find(s => s.CompareTag("En Qi")).text = PlayerPrefs.GetFloat(enQiKey).ToString();
+    }
+    public void IncreaseElijahLvl()
+    {
+        if (elijahLvl < 20)
+        {
+            elijahLvl++;
+            PlayerPrefs.SetFloat(elijahKey, elijahLvl);
+        }
+
+        aiLvlText.Find(s => s.CompareTag("Elijah")).text = PlayerPrefs.GetFloat(elijahKey).ToString();
+    }
+    public void DecreaseElijahLvl()
+    {
+        if (elijahLvl > 0)
+        {
+            elijahLvl--;
+            PlayerPrefs.SetFloat(elijahKey, elijahLvl);
+        }
+
+        aiLvlText.Find(s => s.CompareTag("Elijah")).text = PlayerPrefs.GetFloat(elijahKey).ToString();
     }
 }
